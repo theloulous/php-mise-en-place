1621f30857fea15e904d326f65f7aa3effec2d51
laurie.plociennik <laurie.plociennik@matters.tech>
2019-10-09 20:47:41 +0200

    bufix(test): fix phpunit test

--- src/PhpCsFixer/Configuration.php
+++ src/PhpCsFixer/Configuration.php
@@ -55,6 +55,6 @@ class Configuration
      */
     public function getRule(string $ruleName)
     {
-        return $this->rules[$ruleName];
+        return $this->rules[$ruleName] ?? null;
     }
 }
